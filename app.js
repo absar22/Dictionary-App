5d6a9854c61556a3bea9fc6dfdf9bcf9e73e3556
Absar Ahmad <absarahmad137@gmail.com>
2025-07-05 10:17:57 +0530

    Added new funcnality to my Dicitonary app

--- app.js
+++ app.js
@@ -12,15 +12,36 @@ function getMeaning(){
   fetch(url)
   .then(res => res.json()) // parse response as JSON
   .then(data => {
+    // console.log(data[0].phonetics)
     // console.log(data[0].meanings[0].definitions.map(d => d.definition))
     document.querySelector('#word').innerHTML = data[0].meanings[0].definitions.map(d => d.definition)
 
     
+  
+  document.querySelector('#word').innerHTML = `
+  <strong>Word:</strong> ${data[0].word} <br>
+  <strong>Phonetics:</strong> ${data[0].phonetics[0]?.text || 'N/A'} <br>
+  <strong>Part of Speech:</strong> ${data[0].meanings[0].partOfSpeech} <br>
+  <strong>Definition:</strong> ${data[0].meanings[0].definitions[0].definition} <br>
+  <strong>Example:</strong> ${data[0].meanings[0].definitions[0].example || 'N/A'}
+`    
   })
+
+  
   .catch(err => {
-      console.log(`error ${err}`)
+      // console.log(`error ${err}`)
+      const wordElement = document.querySelector('#word')
+      wordElement.innerText = "Word not found or an error occurred."
+      wordElement.classList.add('warning-red') //add the css file
+      console.error(err)
+    
   });
     
   
 
 }
+
+document.querySelector('input').addEventListener('keypress',function(e){
+  if(e.key === 'Enter') getMeaning()
+  }
+)
